e1d6b999e6a27926170495644be76d4df61d9f39
Haider <malikhaiderkhan81@gmail.com>
2023-07-26 23:47:28 +0500

    add routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import './App.css';
+import { Link, Route, Routes } from 'react-router-dom';
 import Calculator from './components/Calculator';
+import Home from './components/Home';
+import Quote from './components/quote';
 
 function App() {
   return (
-    <Calculator />
+    <>
+      <nav className='navbar'>
+        <ul>
+          <li>
+            <Link to="/Home">Home</Link>
+          </li>
+          <li>
+            <Link to="/Calculator">Calculator</Link>
+          </li>
+          <li>
+            <Link to="/Quote">Quote</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/Home" element={<Home />} />
+        <Route path="/Calculator" element={<Calculator />} />
+        <Route path="/Quote" element={<Quote />} />
+      </Routes>
+    </>
   );
 }
 
